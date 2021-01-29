743446d61fb11ea8fcfad15a720afbd1b636780a
hadi-mirza <hadimirza1@gmail.com>
2021-01-29 17:29:50 -0500

    update paths and class views

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contactus/', views.contact_us, name='contact'),
+    path('clothing/', ClothingList.as_view(), name='clothing_all'),
+    path('clothing/add', ClothingCreate.as_view(), name='clothing_create'),
+    path('clothing/<int:clothing_id>', ClothingDetail.as_view(), name='clothing_detail'),
+    path('clothing/<int:clothing_id>/update', ClothingUpdate.as_view(), name='clothing_update'),
+    path('clothing/<int:clothing_id>/delete', ClothingDelete.as_view(), name='clothing_delete'),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
 def home(request):
   return HttpResponse('ComplimentMyClothes Home Page')
+
+class ClothingList(ListView):
+    model = 
+
+class ClothingDetail(DetailView):
+    model = 
+    pk_url_kwarg = "clothing_id"
+
+class ClothingCreate(CreateView):
+    model = 
+    fields = '__all__'
+    success_url = reverse_lazy('clothing_all')
+    
+class ClothingUpdate(UpdateView):
+    model = 
+    fields = '__all__'
+    pk_url_kwarg = 'clothing_id'
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('clothing_all')
+
+class GameDelete(DeleteView):
+    model = 
+    pk_url_kwarg = 'clothing_id'
+    success_url = reverse_lazy('clothing_all')

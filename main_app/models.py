7345af94d823a1e51427ae961803f7cfb057181e
hadi-mirza <hadimirza1@gmail.com>
2021-01-29 18:33:14 -0500

    add rough models

--- main_app/models.py
+++ main_app/models.py
@@ -2,19 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+COLORS = (
+    ('red', 'Red'),
+    ('blu', 'Blue'),
+    ('ylw', 'Yellow'),
+    ('grn', 'Green'),
+    ('brn', 'Brown')
+    )
+
 class Clothing(models.Model):
     name = models.CharField(max_length=100)
+    color = models.CharField(max_length=1, choices=COLORS, default=COLORS[0][0])
 
     def __str__(self):
         return self.name
 
 class Color(models.Model):
-    name = models.ForeignKey(Clothing, on_delete=models.CASCADE)
+    # color = models.ForeignKey(Clothing, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class ComplimentaryColor(models.Model):
-    colors = 
+    # colors = 
     # def __str__(self):
     #     return self.name

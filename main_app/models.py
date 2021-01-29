881be44cde84585c6500a5d5c3643210cef8f01b
hadi-mirza <hadimirza1@gmail.com>
2021-01-29 18:27:46 -0500

    add rough models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Clothing(models.Model):
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    color = models.ForeignKey(Clothing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class ComplimentaryColor(models.Model):
+    
+    def __str__(self):
+        return self.name
